645be6e1d0f4716fe4b26f86876e3f6c28d59fb3
Utkarsh Tripathi <utkarsh1999tripathi@gmail.com>
2019-10-11 23:55:20 +0530

    Update Checkout.vue

--- components/modal/Checkout.vue
+++ components/modal/Checkout.vue
@@ -78,7 +78,7 @@ export default {
 				} else {
 					productLabel = 'product';
 				}
-				return `Buy ${totalProducts} ${productLabel} at ${finalPrice}&#x20B9;`;
+				return `Buy ${totalProducts} ${productLabel} at ${finalPrice}₹`:
 		},
 		isUserLoggedIn () {
 			return this.$store.getters.isUserLoggedIn;
